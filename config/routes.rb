764102ef8cd68e246c546aa3901a7d4ec8012d4b
mohd497 <likesteel@gmail.com>
2020-02-10 23:26:46 -0800

    routes push origin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "products#index" #index path
   resources :products, only: [:new, :create, :show] #for importing the json file
   get '/softs', to: 'products#all_software'
-  get '/reviews_per_house', to: 'reviews#reviews_per_house'
+  get '/reviews_per_house/:sfthouse_id', to: 'reviews#reviews_per_house'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
